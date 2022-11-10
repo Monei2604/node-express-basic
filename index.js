114cb06114b2bb4043b566abf07a553b175bdbb0
Ngan Do <dongocthanhngan2604@gmail.com>
2022-11-10 23:33:21 +0700

    add user route

--- index.js
+++ index.js
@@ -1,24 +1,70 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+var bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(bodyParser.json({ type: "application/json" }));
 
-app.get('/posts', (req, res) => {
- 
-  const posts = [{
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const users = [
+  {
+    id: "a1",
     firstName: "John",
-    lastName:"Doe"
-  },{
+    lastName: "Doe",
+  },
+  {
+    id: "a2",
     firstName: "John",
-    lastName:"Doe"
-  }]
+    lastName: "Doe",
+  },
+];
 
-  res.json(posts)
-})
+// Get all users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// Get 1 user
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((u) => u.id === id);
+  if (!user) {
+    res.send("User not found");
+    return;
+  }
+  res.json(user);
+});
+
+// Add 1 user
+app.post("/users", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send("Add user success");
+});
+
+// Update a user
+app.patch("/users", (req, res) => {
+  const user = req.body;
+  const index = users.findIndex((u) => u.id === user.id);
+  if(index >=0){
+    users.splice(index, 1, user)
+  }
+  res.send("Update user success");
+});
+
+// Delete user
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const index = users.findIndex((u) => u.id === id);
+  if (index >= 0) {
+    users.splice(index, 1);
+  }
+  res.json("Delete user success");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
